1bf827569ba8aba71169c69ca9e0028473fc6a34
Mao Junjie <eternal.n08@gmail.com>
2012-12-25 22:06:39 +0800

    Setup the project

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject labrepl "0.0.1"
+  :description "Solutions to Project Euler (http://projecteuler.net/) in Clojure"
+  :main euler
+  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :dev-dependencies [[swank-clojure "1.3.0" :exclusions [org.clojure/clojure]]])
---
+++ src/euler.clj
@@ -0,0 +1,4 @@
+(ns euler)
+
+(defn -main [& args]
+  (println "Usage: lein run -m euler/problem-###"))
