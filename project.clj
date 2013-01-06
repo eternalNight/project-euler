7a34c7395466ab1ff13e234d22a78493dfe81238
Mao Junjie <eternal.n08@gmail.com>
2013-01-06 12:16:36 +0800

    Add clojure.contrib to dependency list in project.clj
    Signed-off-by: Junjie Mao <eternal.n08@gmail.com>

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject euler "0.0.1"
   :description "Solutions to Project Euler (http://projecteuler.net/) in Clojure"
   :main euler
-  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[swank-clojure "1.3.0" :exclusions [org.clojure/clojure]]])
